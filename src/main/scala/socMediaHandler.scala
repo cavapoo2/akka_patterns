12194b17d89455512391199f6b29093312502d57
andyr <forpetessake@protonmail.com>
2017-09-30 16:41:34 +0100

    Add files via upload

---
+++ src/main/scala/socMediaHandler.scala
@@ -0,0 +1,15 @@
+package com.andyr
+import akka.actor.{Actor, ActorLogging}
+import com.andyr.SocialMediaAggregator.{GetLatestPosts, LatestPostResult, Post}
+
+import scala.util.Random
+
+class SocialMediaHandler(socialMedia: String) extends Actor with ActorLogging {
+
+  def receive = {
+    case GetLatestPosts(id) =>
+      Thread.sleep(Random.nextInt(4) * 100)
+      val posts = (0 to Random.nextInt(2)).map(_ => Post("some title", "some content"))
+      sender ! LatestPostResult(socialMedia, posts)
+  }
+}
