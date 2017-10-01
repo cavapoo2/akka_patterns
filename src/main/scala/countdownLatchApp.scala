8cc94d0ecfa4eacde9b98306a339d39cd12156c9
andyr <forpetessake@protonmail.com>
2017-10-01 19:55:23 +0100

    Add files via upload

---
+++ src/main/scala/countdownLatchApp.scala
@@ -0,0 +1,20 @@
+package com.andyr
+import akka.actor.{ActorSystem, Props}
+import akka.routing.RoundRobinPool
+
+object CountDownLatchApp extends App {
+  implicit val actorSystem = ActorSystem()
+  import actorSystem._
+  val routeesToSetUp = 10
+  val countDownLatch = CountDownLatch(routeesToSetUp) // a class with inbuilt actor and future handling
+  //we have one CountDownLatch but 10 CountDownLatchWorkers
+  actorSystem.actorOf(Props(classOf[CountDownLatchWorker], countDownLatch)   //worker actor that uses it
+    .withRouter(RoundRobinPool(routeesToSetUp)), "workers")
+
+  //Future based solution
+  countDownLatch.result.onSuccess { case _ => log.info("Future completed successfully") }
+
+  //Await based solution
+  countDownLatch.await()
+  actorSystem.terminate()
+}
---
+++ src/main/scala/countdownLatchWorker.scala
@@ -0,0 +1,17 @@
+package com.andyr
+import akka.actor.{Actor, ActorLogging}
+
+import scala.util.Random
+
+class CountDownLatchWorker(countDownLatch: CountDownLatch) extends Actor with ActorLogging {
+
+  override def preStart() = {
+    //do something
+    val millis = Random.nextInt(5) * 100
+    log.info(s"Sleeping $millis millis")
+    Thread.sleep(millis)
+    countDownLatch.countDown()
+  }
+
+  def receive = Actor.ignoringBehavior
+}
